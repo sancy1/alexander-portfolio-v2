a72ec1b4138c4018065aa6e876c5acdc1ff47bfb
sancy1 <sanchez.alexander.cyril@gmail.com>
2026-05-26 23:23:33 +0100

    fix: Use internal Render network with separate host and port variables

--- api-gateway/docker-entrypoint.sh
+++ api-gateway/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
-envsubst '${AUTH_SERVICE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "=== AUTH_SERVICE_HOST: '${AUTH_SERVICE_HOST}' ==="
+echo "=== AUTH_SERVICE_PORT: '${AUTH_SERVICE_PORT}' ==="
+
+envsubst '${AUTH_SERVICE_HOST} ${AUTH_SERVICE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+echo "=== nginx.conf sample ==="
+sed -n '1,30p' /etc/nginx/nginx.conf
 
 exec "$@"

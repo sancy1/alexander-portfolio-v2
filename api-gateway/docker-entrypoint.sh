35adbb1f12b2b810ecec8d380d406c44d35b97f1
sancy1 <sanchez.alexander.cyril@gmail.com>
2026-05-26 22:54:00 +0100

    fix: Implement correct gateway routing per research

--- api-gateway/docker-entrypoint.sh
+++ api-gateway/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# Replace environment variables in nginx config
-envsubst '${AUTH_SERVICE_URL} ${AUTH_SERVICE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${AUTH_SERVICE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Execute the main command
 exec "$@"

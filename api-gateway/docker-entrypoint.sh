d4fbd4c930e7856f2ad031d1afa19c745b438cd9
sancy1 <sanchez.alexander.cyril@gmail.com>
2026-05-27 14:10:48 +0100

    fix: Strip the headers that trigger WAF

--- api-gateway/docker-entrypoint.sh
+++ api-gateway/docker-entrypoint.sh
@@ -1,12 +1,31 @@
+# #!/bin/sh
+# set -e
+
+# echo "=== AUTH_SERVICE_HOST: '${AUTH_SERVICE_HOST}' ==="
+# echo "=== AUTH_SERVICE_PORT: '${AUTH_SERVICE_PORT}' ==="
+
+# envsubst '${AUTH_SERVICE_HOST} ${AUTH_SERVICE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# echo "=== nginx.conf sample ==="
+# sed -n '1,30p' /etc/nginx/nginx.conf
+
+# exec "$@"
+
+
+
+
+
+
+
+
+
 #!/bin/sh
 set -e
 
 echo "=== AUTH_SERVICE_HOST: '${AUTH_SERVICE_HOST}' ==="
-echo "=== AUTH_SERVICE_PORT: '${AUTH_SERVICE_PORT}' ==="
 
-envsubst '${AUTH_SERVICE_HOST} ${AUTH_SERVICE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${AUTH_SERVICE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-echo "=== nginx.conf sample ==="
-sed -n '1,30p' /etc/nginx/nginx.conf
+echo "=== Proxying to: https://${AUTH_SERVICE_HOST} ==="
 
 exec "$@"
